4c0801bc1fe7ea86cac35380b53347df081a01b3
umialpha <roy236977634@gmail.com>
2020-05-12 01:08:57 +0800

    modify grpc as stream

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOOTSTRAP_SERVER = "172.16.0.5:9092"
 JOB_SUBMIT_TOPIC = "kafka-poc-job-submit-"
 JOB_FINISH_TOPIC = "kafka-poc-job-finish-"
-JOB_ID = 1011
+JOB_ID = 1012
 TASK_NUM = 10**6 
 WORKERS_ADDRS = ["172.16.0.8:{0}".format(i) for i in range(5001, 5001 + 32)]
