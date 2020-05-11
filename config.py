15062d0686735422c3227d1078e8fca514a3595f
umialpha <roy236977634@gmail.com>
2020-05-11 21:52:51 +0800

    bugfix

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOOTSTRAP_SERVER = "172.16.0.5:9092"
 JOB_SUBMIT_TOPIC = "kafka-poc-job-submit-"
 JOB_FINISH_TOPIC = "kafka-poc-job-finish-"
-JOB_ID = 1002
+JOB_ID = 1003
 TASK_NUM = 10**5
 WORKERS_ADDRS = ["172.16.0.7:5001","172.16.0.7:5002", "172.16.0.7:5003"]
